dbf3e46ab47c522fea8489d87cad4cc1c983b5bb
wendywu0313 <s1100383@gm.pu.edu.tw>
2022-10-18 14:45:18 +0800

    make it better

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    homepage = "<h1>楊子青Python+flask+Vercel網頁</h1>"
+    homepage = "<h1>吳若耶Python+flask+Vercel網頁</h1>"
     homepage += "<a href=/mis>MIS</a><br>"
     homepage += "<a href=/today>顯示日期時間</a><br>"
     homepage += "<a href=/welcome?nick=tcyang>傳送使用者暱稱</a><br>"
